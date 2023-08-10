76f87ec27c93f17f50414ddc543973bda2fee02d
TheHabibi <celebialieren@gmail.com>
2023-08-11 02:10:00 +0300

    dockerize

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("colors");
-const port = 3000;
+const port = 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello Life Fish!");
